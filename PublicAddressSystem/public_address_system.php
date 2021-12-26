75c700af7d098991150a269dbc35ae3109114e86
priyaprem <priyaprem08@gmail.com>
2021-12-26 18:15:23 +0530

    css changes

--- PublicAddressSystem/public_address_system.php
+++ PublicAddressSystem/public_address_system.php
@@ -16,56 +16,56 @@ if(!$con)
     <head>
 	<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
         <title>Public Address System</title>
+		<style type="text/css">
+	table.imagetable {
+		font-family: verdana,arial,sans-serif;
+		font-size:11px;
+		color:#333333;
+		border-width: 1px;
+		border-color: #999999;
+		border-collapse: collapse;
+	}
+	table.imagetable th {
+		background:#b5cfd2 url('cell-blue.jpg');
+		border-width: 1px;
+		padding: 8px;
+		border-style: solid;
+		border-color: #999999;
+	}
+	table.imagetable td {
+		background:#dcddc0 url('cell-grey.jpg');
+		border-width: 1px;
+		padding: 8px;
+		border-style: solid;
+		border-color: #999999;
+		word-wrap: break-word
+	}
+	</style>
     </head>
 	<!-- CSS goes in the document HEAD or added to your external stylesheet -->
-<style type="text/css">
-table.imagetable {
-	font-family: verdana,arial,sans-serif;
-	font-size:11px;
-	color:#333333;
-	border-width: 1px;
-	border-color: #999999;
-	border-collapse: collapse;
-}
-table.imagetable th {
-	background:#b5cfd2 url('cell-blue.jpg');
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #999999;
-}
-table.imagetable td {
-	background:#dcddc0 url('cell-grey.jpg');
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #999999;
-}
-</style>
+
 <!-- Table goes in the document BODY -->
 
-    <body onload="javascript:pageloadEvery(30000);">
+<body onload="javascript:pageloadEvery(30000);">
 
 	<center><h1>PUBLIC ADDRESS SYSTEM</h1></center>
 	<table class="imagetable" align="center">
-<tr>
-	<th>MESSAGE</th><th>ID</th><th>PUBLISHED AT</th>
-</tr>
+		<tr>
+			<th>MESSAGE</th><th>ID</th><th>PUBLISHED AT</th>
+		</tr>
 <!-- We use while loop to fetch data and display rows of date on html table -->
 
-<?php
+			<?php
 
-     while ($message = mysqli_fetch_assoc($records)){
-           echo "<tr>";
-               echo "<td>".$message['message']."</td>";
-               echo "<td>".$message['id']."</td>";
-			   echo "<td>".$message['created_at']."</td>";
-           echo "</tr>";
+				while ($message = mysqli_fetch_assoc($records)){
+					echo "<tr>";
+						echo "<td>".$message['message']."</td>";
+						echo "<td>".$message['id']."</td>";
+						echo "<td>".$message['created_at']."</td>";
+					echo "</tr>";
 
-     }
-?>
-        </table>
-
+				}
+			?>
+    </table>
    </body>
-
 </html>
